67f7394ad8d68c8a07692cdc666cda706b4e82ce
ryo.fukushima <ryo.fukushima@firstloop-tech.com>
2021-09-05 03:09:48 +0900

    feat: 🎸 init

---
+++ sample.js
@@ -0,0 +1,27 @@
+async function postData(url = '', data = {}) {
+  console.log(data)
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data)
+  })
+  return response.json();
+}
+
+async function getIp() {
+  await fetch('https://ipinfo.io?callback')
+    .then(res => res.json())
+    .then(json => ip =json.ip)
+  return ip
+}
+
+
+getIp().then(ip => {
+  postData("http://localhost:8000/v1/refresh/", { time: new Date(), location: window.location.href, ip: ip })
+  .then(data => {
+    console.log(data);
+  });  
+})
+
